a775c75dae1c03fa2a6875ec6c322e1cd74b6c11
Matias Bragadini <m_bragadini@hotmail.com>
2022-04-04 12:47:18 -0300

    Corrijo error

--- Dia1/src/Inciso5.java
+++ Dia1/src/Inciso5.java
@@ -1,5 +1,4 @@
 public class Inciso5 {
-    public class Inciso4 {
         public static void main(String[] args) {
             double val1 = 3;
             double val2 = 2;
@@ -14,6 +13,3 @@ public class Inciso5 {
         }
     }
 
-
-
-}
