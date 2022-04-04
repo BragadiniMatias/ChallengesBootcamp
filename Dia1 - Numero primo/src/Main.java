244a8025598449db1ccc38a5e4575567da4991fe
Matias Bragadini <m_bragadini@hotmail.com>
2022-04-04 15:32:23 -0300

    commit

--- Dia1 - Numero primo/src/Main.java
+++ Dia1 - Numero primo/src/Main.java
@@ -11,9 +11,7 @@ public class Main {
 
 
     }
-    //Como no sabia si la idea era usar un ciclo for o no, lo deje bastante hard=codeado para que sea exclusivamente
-    //con IF y SWITCH. Usando ciclos for o whjile se puede limpiar  muchisimo mas y se que esta forma no es la optima pero
-    //lo dejo asi para seguir al pie de la letra la consigna. .
+
     public static String primeChecker(int num){
         if(num ==1 || num ==2||num ==3 || num ==5 ||num ==7 || num ==11 ||num ==13){
             return "Es primo";
@@ -23,13 +21,7 @@ public class Main {
     }
     public static String switchPrimeChecker(int num){
         switch(num){
-            case 1: return "Es primo";
-            case 2: return "Es primo";
-            case 3: return "Es primo";
-            case 5: return "Es primo";
-            case 7: return "Es primo";
-            case 11: return "Es primo";
-            case 13: return "Es primo";
+            case 1,2,3,5,7,11,13: return "Es primo";
             default: return "No es primo";
 
 
