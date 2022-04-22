b04f52c019ed36ba4e3b3794d7ed559bae7c76d7
BragadiniMatias <101947035+BragadiniMatias@users.noreply.github.com>
2022-04-22 18:04:42 -0300

    Add files via upload

---
+++ Dia 14/DevPlaceDia14/DevPlaceDia15/src/test/java/com/matias/DevPlaceDia15/DevPlaceDia15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.DevPlaceDia15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevPlaceDia15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
